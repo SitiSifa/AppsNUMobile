c1370475fd3d6bf228c33b20879d9de1023d2dba
SitiSIfa <siti.sitisifa@gmail.com>
2017-09-26 14:26:04 +0700

    integration BTPN WOW

---
+++ app/src/main/java/com/NU/numobile/activity/banking/BankingBTPN.java
@@ -0,0 +1,51 @@
+package com.NU.numobile.activity.banking;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+import com.NU.numobile.R;
+
+public class BankingBTPN extends AppCompatActivity {
+
+    private WebView view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_banking_btpn);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        view = (WebView) this.findViewById(R.id.webview);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("https://btpnwow.btpn.com/");
+
+    }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+        public boolean onKeyDown(int keyCode, KeyEvent event) {
+            //ketika disentuh tombol back
+            if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+                view.goBack(); //method goback(),untuk kembali ke halaman sebelumnya
+                return true;
+            }
+            // Jika tidak ada halaman yang pernah dibuka
+            // maka akan keluar dari activity (tutup aplikasi)
+            return super.onKeyDown(keyCode, event);
+        }
+    }
--- app/src/main/java/com/NU/numobile/activity/banking/MenuBanking.java
+++ app/src/main/java/com/NU/numobile/activity/banking/MenuBanking.java
@@ -1,12 +1,15 @@
 package com.NU.numobile.activity.banking;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
+import com.NU.numobile.MenuUtama;
 import com.NU.numobile.R;
 
 public class MenuBanking extends AppCompatActivity {
@@ -17,6 +20,15 @@ public class MenuBanking extends AppCompatActivity {
         setContentView(R.layout.activity_menu_banking);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button btpn = (Button) findViewById(R.id.btpn);
+        btpn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MenuBanking.this, BankingBTPN.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
